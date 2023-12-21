441361968c49472b170a602dbe44db69c3955f42
Maxence Gtn <saegitlabmjct@proton.me>
2023-12-21 12:45:41 +0000

    Incrémentation l1, test ping testHostOnly

--- tests/tests/GetRequestTest.php
+++ tests/tests/GetRequestTest.php
@@ -2,29 +2,14 @@
 
 use PHPUnit\Framework\TestCase;
 
-require_once __DIR__ '/../../scripts/get_request.php'; // Chemin vers le fichier contenant la fonction
+require_once __DIR__ . '/../../scripts/get_request.php'; // Chemin vers le fichier contenant la fonction
 
 final class GetRequestTest extends TestCase
 {
-    public function testGetRequestWithValidArguments(): void
+    public function testHostOnly(): void
     {
-        $expected = 'ping -c 5 example.com -q -D';
-        $result = get_request('example.com', 5, true, true);
+        $expected = 'ping -c 3 example.com';
+        $result = get_request('example.com', 0, false, false);
         $this->assertEquals($expected, $result);
-    }
-
-    public function testGetRequestWithNoCountArgument(): void
-    {
-        $expected = 'ping example.com -q -D';
-        $result = get_request('example.com', 0, true, true);
-        $this->assertEquals($expected, $result);
-    }
-
-    public function testGetRequestWithEmptyHostThrowsException(): void
-    {
-        $this->expectException(Exception::class);
-        $this->expectExceptionMessage('HOST vide');
-        get_request('', 5, true, true);
     }
 }
-
