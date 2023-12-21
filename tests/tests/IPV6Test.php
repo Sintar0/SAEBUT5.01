17c10c873b10c03a8de7ec733ebf3b8fe5a20a85
Maxence Gtn <saegitlabmjct@proton.me>
2023-12-21 15:10:34 +0000

    testGlobalUnicast2

--- tests/tests/IPV6Test.php
+++ tests/tests/IPV6Test.php
@@ -16,4 +16,15 @@ final class IPV6Test extends TestCase
         $this->assertEquals($expectedType, $resultType);
         $this->assertEquals($expectedSimplification, $resultSimplify);
     }
+
+    public function testGlobalUnicast2(): void
+    {
+        $expectedSimplification = '2001:db8:ab00:cd00:ef00::12';
+        $expectedType = 'Global Unicast';
+        $resultSimplify = simplify_ipv6('2001:0DB8:AB00:CD00:EF00:0000:0000:0012');
+        $resultType = get_type_ipv6('2001:0DB8:AB00:CD00:EF00:0000:0000:0012');
+        $this->assertEquals($expectedType, $resultType);
+        $this->assertEquals($expectedSimplification, $resultSimplify);
+    }
+
 }
