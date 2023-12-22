9da2fa271382197502d38d7372ee2b858c5ab29b
Samuel <samchgaming@gmail.com>
2023-12-22 11:27:56 +0100

    Ajout logo de module pour accessibilité

--- component/navbar_module.php
+++ component/navbar_module.php
@@ -1,6 +1,6 @@
 <nav>
     <a href="../../index.php"  id="logo">
-        <img id="logo_img" src="../../img/logo.png" alt="logo_maquette" srcset="">
+        <img id="logo_img" src="../../img/logo_module.png" alt="logo_maquette" srcset="">
     </a>
 </nav>
 
